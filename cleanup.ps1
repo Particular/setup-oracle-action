7483052773678cf7ce457f0d2c91267967978b5b
Daniel Marbach <daniel.marbach@openplace.net>
2022-06-21 11:52:25 +0200

    Make sure the cleanup script also removes linux containers This does not only come in handy for local testing but also makes sure the ports are freed up in case the setup action is used multiple times

--- cleanup.ps1
+++ cleanup.ps1
@@ -10,3 +10,10 @@ if ($Env:RUNNER_OS -eq "Windows") {
     Write-Output "Deleting Azure storage account $StorageName"
     az storage account delete --resource-group GitHubActions-RG --name $StorageName --yes | Out-Null
 }
+else {
+    Write-Output "Force stopping Docker container $ContainerName"
+    docker kill $ContainerName
+
+    Write-Output "Removing Docker container $ContainerName"
+    docker rm $ContainerName
+}
